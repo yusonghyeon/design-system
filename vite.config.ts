c599d39af27e4a995ba81b81528cd37bc0575520
yusonghyeon <ysh2987@naver.com>
2024-12-18 18:48:32 +0900

    chore: vanilla-extract 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vanillaExtractPlugin()],
   test: {
     globals: true,
     environment: 'jsdom',
